1ede72b43641f6ff61a785c3926b34bef742fd93
Luke Alousis <lucasalousis@outlook.com>
2018-08-22 19:38:30 -0400

    Deployed to Rinkeby

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@
 //const mnemonic = "[YOUR MNEMONIC]"
 //to use pk and not mnemonic
 var PrivateKeyProvider = require("truffle-privatekey-provider");
-const privKey = "[YOUR PRIVATE KEY]"
+const privKey = "AA7F1635378B893F765A89AE8EF55C167DB503929C8E395B9A30D9B4D3068C19"
 
 //latest deployed contract address: 0xdac2ff7c887cd14cde7e6e8411f6b40eba1f3cc1
 //on rinkeby
@@ -17,7 +17,7 @@ module.exports = {
     rinkeby: {
       provider: function() {
         //return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/[YOUR INFURA TOKEN HERE]")
-        return new PrivateKeyProvider(privKey, 'https://rinkeby.infura.io/[YOUR INFURA TOKEN HERE]')
+        return new PrivateKeyProvider(privKey, 'https://rinkeby.infura.io/v3/95494cec27284b7eadfcc3804d34744d')
       },
       network_id: '4'
     }
